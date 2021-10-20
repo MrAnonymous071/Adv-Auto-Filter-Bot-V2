609248cede4a5fecd37cd3a99d53c2f74ef95406
MrAnonymous071 <88572086+MrAnonymous071@users.noreply.github.com>
2021-10-20 20:12:29 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code> \n \n<b>Join :🔗@Bucket_LinkZz🔗 </b>"
+    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code> \n \n<b>Join :🔗 @Bucket_LinkZz 🔗 </b>"
         
     try:
         await update.message.edit(
